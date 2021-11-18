1bf107d6feb0f48ffcdd3b7c8912d2c973ac9e4c
Martin Fuentes <mefuentes@uninorte.edu.co>
2021-11-17 21:53:23 -0500

    [ADD] Basic types

---
+++ Basic-types.exs
@@ -0,0 +1,20 @@
+# Data types
+# 1          # integer
+# 0x1F       # integer
+# 1.0        # float
+# true       # boolean
+# :atom      # atom / symbol
+# "elixir"   # string
+# [1, 2, 3]  # list
+# {1, 2, 3}  # tuple
+
+_intType = 1
+floatType = 3.14159265359
+_floatScientificType = 1.0e-10
+_booleanType = true
+_atomType = :atom
+_stringType = "Hello world!"
+_listType = {1, 2, 3}
+_tupleType = [1, 2, 3]
+
+IO.puts(trunc(floatType))
