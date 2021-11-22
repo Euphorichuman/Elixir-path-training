2f38a4cece989f44ae157344d6020529c1cac989
Martin Fuentes <mefuentes@uninorte.edu.co>
2021-11-22 10:33:37 -0500

    [FIX] camelCase to snake_case

--- KeywordList-Maps.exs
+++ KeywordList-Maps.exs
@@ -1,21 +1,21 @@
 # Keyword Lists
-kList = [{:a, "Hello"}, {:b, "world"}]
-IO.puts(inspect(kList))
+k_list = [{:a, "Hello"}, {:b, "world"}]
+IO.puts(inspect(k_list))
 
-kList2 = kList ++ [c: "!"]
+kList2 = k_list ++ [c: "!"]
 IO.puts(inspect(kList2))
 
 # Maps
 map = %{:a => "Hello", "World" => :b}
 x = 1
-map2 = %{x => :atom}
-IO.puts(map2[x])
+map_2 = %{x => :atom}
+IO.puts(map_2[x])
 
 IO.puts(Map.get(map, :a))
-map3 = Map.put(map, :c, "!")
-IO.puts(inspect(map3))
-map4 = %{map3 | :c => " "}
-IO.puts(inspect(map4.c))
+map_3 = Map.put(map, :c, "!")
+IO.puts(inspect(map_3))
+map_4 = %{map_3 | :c => " "}
+IO.puts(inspect(map_4.c))
 
 # Nested data structures
 users = [
